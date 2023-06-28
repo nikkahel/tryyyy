0b6aeaec282bfc1a3b17cfd718a8ac3899405fb1
nikkahel <n.k.copy1@gmail.com>
2023-06-28 13:23:58 +0300

    Newdb

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const { Pool, Client } = require("pg")
+const { Pool } = require("pg")
 require('dotenv').config()
 
 const app = express()
 app.use(express())
 const PORT = process.env.PORT || 3000
+const connectDb = async () => {
+    try {
+        const pool = new Pool({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT,
+        });
+     await pool.connect()
+
+        await pool.end()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connectDb()
 app.get('/', (req, res) =>{
     res.send('Hello world')
 })
