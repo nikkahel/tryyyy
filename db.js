a8e79df2251b40354c307ce612e5106d66a43f3c
nikkahel <n.k.copy1@gmail.com>
2023-06-29 16:52:58 +0300

    check

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const dbConnection = async () =>{
     try{
         const itemsPool = new Pool({
-            connectionString: process.env.DBCONFIGLINK,
+            connectionString: `postgres://projectdb_ttpq_user:HUIvZQQl9eUve1sl4Zad7fUDXbTUj2RL@dpg-cidtdcp8g3n4p2ppcin0-a.frankfurt-postgres.render.com/projectdb_ttpq`,
             ssl: {
                 rejectUnauthorized: false
             }
